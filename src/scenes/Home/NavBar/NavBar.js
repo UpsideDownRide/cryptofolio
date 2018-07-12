138d1d37fe28f59ece97d2f302a80107d7621a9c
Radomir Nowacki <kenderpl@gmail.com>
2018-07-12 19:41:21 +0200

    Barebones navbar

---
+++ src/scenes/Home/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import style from './NavBar.module.css'
+
+const navBar = () => {
+    const notImplemented = () => alert('not implemented')
+    return (
+        <nav className={style.nav}>
+            <ul className={style.ul}>
+                <li>Cryptofolio</li>
+                <li><a href="#" onClick={notImplemented}>Login</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default navBar
