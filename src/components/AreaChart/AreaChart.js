f9946a950e73360671a5064d6e193d705985b3e3
Radomir Nowacki <kenderpl@gmail.com>
2018-07-15 03:52:00 +0200

    GraphPanes reactive

--- src/components/AreaChart/AreaChart.js
+++ src/components/AreaChart/AreaChart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { ResponsiveContainer, AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 const data = [
     { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
@@ -13,25 +13,27 @@ const data = [
 
 const areaChart = () => {
     return (
-        <AreaChart width={280} height={200} data={data}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-            <defs>
-                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-                </linearGradient>
-                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                    <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-                </linearGradient>
-            </defs>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip />
-            <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-            <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
-        </AreaChart>
+        <ResponsiveContainer height={200}>
+            <AreaChart width={280} height={200} data={data}
+                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                <defs>
+                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                    </linearGradient>
+                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                    </linearGradient>
+                </defs>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+            </AreaChart>
+        </ResponsiveContainer>
     )
 }
 
--- src/components/TreeMap/TreeMap-Recharts.js
+++ src/components/TreeMap/TreeMap-Recharts.js
@@ -1,64 +1,84 @@
-import React from 'react'
-import { Treemap } from 'recharts'
+import  React from 'react'
+import { Treemap, ResponsiveContainer } from 'recharts'
+
 
 const data = [
-            { name: 'BTC', size: 5000 },
-            { name: 'ETH', size: 3000 },
-            { name: 'ADA', size: 2000 },
-            { name: 'AMB', size: 1000 },
-            { name: 'DOGE', size: 1000 },
+    { name: 'BTC', size: 5000 },
+    { name: 'ETH', size: 3000 },
+    { name: 'ADA', size: 2000 },
+    { name: 'AMB', size: 1000 },
+    { name: 'DOGE', size: 1000 },
 ];
 
-const COLORS = ['cadetblue','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9'];
+const COLORS = ['cadetblue', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9'];
 
 const CustomizedContent = ({ root, depth, x, y, width, height, index, payload, colors, rank, name }) => {
-        return (
-            <g>
-                <rect
-                    x={x}
-                    y={y}
-                    width={width}
-                    height={height}
-                    style={{
-                        fill: depth < 2 ? colors[Math.floor(index / root.children.length * 6)] : 'none',
-                        stroke: '#fff',
-                        strokeWidth: 2 / (depth + 1e-10),
-                        strokeOpacity: 1 / (depth + 1e-10),
-                    }}
-                />
-                {
-                    depth === 1 ?
-                        <text
-                            x={x + width / 2}
-                            y={y + height / 2 + 7}
-                            textAnchor="middle"
-                            fill="#fff"
-                            fontSize={14}
-                        >
-                            {name}
-                        </text>
-                        : null
-                }
-            </g>
-        );
+    return (
+        <g>
+            <rect
+                x={x}
+                y={y}
+                width={width}
+                height={height}
+                style={{
+                    fill: depth < 2 ? colors[Math.floor(index / root.children.length * 6)] : 'none',
+                    stroke: '#fff',
+                    strokeWidth: 2 / (depth + 1e-10),
+                    strokeOpacity: 1 / (depth + 1e-10),
+                }}
+            />
+            {
+                depth === 1 ?
+                    <text
+                        x={x + width / 2}
+                        y={y + height / 2 + 7}
+                        textAnchor="middle"
+                        fill="#fff"
+                        fontSize={14}
+                    >
+                        {name}
+                    </text>
+                    : null
+            }
+        </g>
+    );
 };
 
-const SimpleTreemap = () => {
+function once(fn, context) { 
+	var result;
+
+	return function() { 
+		if(fn) {
+			result = fn.apply(context || this, arguments);
+			fn = null;
+		}
+
+		return result;
+	};
+}
+
+const initialWidth = once(() => 300) || "100%"
+
+class SimpleTreemap extends React.Component {
+    render() {
         return (
-            <Treemap
-                width={280}
-                height={200}
-                data={data}
-                dataKey="size"
-                ratio={(1 + Math.sqrt(5)) / 2}
-                stroke="#fff"
-                fill="#8884d8"
-                content={<CustomizedContent colors={COLORS}/>}
-                isAnimationActive={true}
-                animationDuration={600}
-                animationEasing="ease-in-out"
-            />
-        );
+            <ResponsiveContainer width={"100%"} height={200}>
+                <Treemap
+                    width={300}
+                    height={200}
+                    data={data}
+                    dataKey="size"
+                    ratio={(1 + Math.sqrt(5)) / 2}
+                    stroke="#fff"
+                    fill="#8884d8"
+                    content={<CustomizedContent colors={COLORS} />}
+                    isAnimationActive={false}
+                    animationDuration={600}
+                    animationEasing="ease"
+                />
+            </ResponsiveContainer>
+        )
+    }
 }
 
 export default SimpleTreemap
