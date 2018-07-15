ee230bffcb810d0488077c60f9f1a43631e7ba2d
Radomir Nowacki <kenderpl@gmail.com>
2018-07-15 09:54:40 +0200

    Added empty Login and Portfolio

--- src/scenes/Home/Main/Main.js
+++ src/scenes/Home/Main/Main.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Dashboard from '../Dashboard/Dashboard'
+import Portfolio from '../Portfolio/Portfolio'
+import Login from '../Login/Login'
 
 const main = () => (
     <React.Fragment>
         <Switch>
             <Route exact path="/" component={Dashboard} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/login" component={Login} />
         </Switch>
     </React.Fragment>
 )
-
 
 export default main
