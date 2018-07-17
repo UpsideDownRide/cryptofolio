184ce9206eef9fecffbd04ee6096e02db0314c1b
Radomir Nowacki <kenderpl@gmail.com>
2018-07-17 17:46:31 +0200

    Separated transaction form from modal

---
+++ src/scenes/Home/Portfolio/AddTransactionForm.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from 'moment'
+import { Dropdown, Form, Segment } from 'semantic-ui-react'
+
+const countryOptions = [{ key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' }]
+
+const DropdownExampleSearchSelection = () => (
+    <Dropdown placeholder='Select Country' fluid search selection options={countryOptions} />
+)
+
+export default class AddTransactionForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            startDate: moment()
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        });
+    }
+
+    render() {
+        return (
+            <Form>
+                <Form.Field>
+                    <label>Transaction Type:</label>
+                    <DropdownExampleSearchSelection />
+                </Form.Field>
+                <Form.Group>
+                    <Form.Field>
+                        <label>Enter Date:</label>
+                        <DatePicker
+                            selected={this.state.startDate}
+                            onChange={this.handleChange}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Enter time:</label>
+                        <input></input>
+                    </Form.Field>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Field>
+                        <label>Amount:</label>
+                        <input />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Amount:</label>
+                        <DropdownExampleSearchSelection />
+                    </Form.Field>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Field>
+                        <label>Amount:</label>
+                        <input />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Amount:</label>
+                        <DropdownExampleSearchSelection />
+                    </Form.Field>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Field>
+                        <label>Amount:</label>
+                        <input />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Amount:</label>
+                        <DropdownExampleSearchSelection />
+                    </Form.Field>
+                </Form.Group>
+                <Form.Group>
+
+                    <Form.Field>
+                        <label>Amount:</label>
+                        <input />
+                    </Form.Field>
+                </Form.Group>
+            </Form>
+        )
+    }
+}
--- src/scenes/Home/Portfolio/Modal.js
+++ src/scenes/Home/Portfolio/Modal.js
@@ -1,24 +1,21 @@
 import React from 'react'
-import { Dropdown, Button, Header, Segment, Modal, Icon } from 'semantic-ui-react'
+import { Button, Modal, Icon } from 'semantic-ui-react'
 
-const countryOptions = [ { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' } ]
-
-const DropdownExampleSearchSelection = () => (
-    <Dropdown placeholder='Select Country' fluid search selection options={countryOptions} />
-)
+import AddTransactionForm from './AddTransactionForm'
 
 
-const ModalModalExample = () => (
-    <Modal trigger={<Button positive><Icon name='plus' />Add Transaction</Button>}>
-        <Modal.Header style={{backgroundColor: "#21BA45", color: "#FFFFFF"}}>Add a new transaction</Modal.Header>
-        <Modal.Content>
-            <Segment>Type and Date</Segment>
-            <Segment>Buy</Segment>
-            <Segment>Sell</Segment>
-            <Segment>Fee</Segment>
-            <Segment>Details</Segment>
-        </Modal.Content>
-    </Modal>
-)
+class ModalModalExample extends React.Component {
+
+    render() {
+        return <Modal trigger={<Button positive><Icon name='plus' />Add Transaction</Button>}>
+            <Modal.Header style={{ backgroundColor: "#21BA45", color: "#FFFFFF" }}>Add a new transaction</Modal.Header>
+            <Modal.Content>
+                <AddTransactionForm/>
+                <Button positive>Add</Button>
+            </Modal.Content>
+        </Modal>
+
+    }
+}
 
 export default ModalModalExample
