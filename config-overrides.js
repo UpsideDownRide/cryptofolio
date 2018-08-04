6cb5c85ba126175186d8c83168d1e00dc8ce8500
Radomir Nowacki <kenderpl@gmail.com>
2018-08-05 00:32:22 +0200

    rewire hot loader

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,8 @@ const { compose } = require('react-app-rewired')
 
 module.exports = {
     webpack: compose(
-        require('react-app-rewire-css-modules-extensionless').webpack({ /* options */ })
+        require('react-app-rewire-css-modules-extensionless').webpack({ /* options */ }),
+        require('react-app-rewire-hot-loader')
     ),
     jest: compose(
         require('react-app-rewire-css-modules-extensionless').jest()
