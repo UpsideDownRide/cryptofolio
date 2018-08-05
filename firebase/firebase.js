4904f112d98121f4439548194209ce770da4ee96
Radomir Nowacki <kenderpl@gmail.com>
2018-08-05 07:47:50 +0200

    firebase directory

---
+++ firebase/firebase.js
@@ -0,0 +1,12 @@
+<script>
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAVab6zQjti5EK7T9GxE5gKOcEBInkkyJ0",
+    authDomain: "havencrypto.firebaseapp.com",
+    databaseURL: "https://havencrypto.firebaseio.com",
+    projectId: "havencrypto",
+    storageBucket: "havencrypto.appspot.com",
+    messagingSenderId: "728450041717"
+  };
+  firebase.initializeApp(config);
+</script>
