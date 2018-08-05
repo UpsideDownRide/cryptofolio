caaec5feeab92bdbd186a31e031a28dc1a2c814b
Radomir Nowacki <kenderpl@gmail.com>
2018-08-05 04:54:22 +0200

    Landing page

--- src/scenes/LandingPage/LandingPage.js
+++ src/scenes/LandingPage/LandingPage.js
@@ -1,17 +1,45 @@
 import React from 'react'
-import { Container, Segment, Header, Icon } from 'semantic-ui-react';
+import { Segment, Header, Grid, Button } from 'semantic-ui-react';
+import style from './LandingPage.module.css'
+import ContentWrapper from 'components/ContentWrapper/ContentWrapper'
 
 const LandingPage = () => {
     return (
-        <Container as="section">
-            <Segment>
-                <Header>
-                <Icon name="anchor" circular />
-                <Header.Content>Welcome to Cryptohaven</Header.Content>
-                </Header>
-
-            </Segment>
-        </Container>
+        <div className={style.landing}>
+            <ContentWrapper>
+                <Grid centered padded relaxed='very'>
+                    <Grid.Row />
+                    <Grid.Row>
+                        <Grid.Column />
+                        <Grid.Column width={10}>
+                            <Segment vertical massive padded='very'>
+                                <Header inverted>
+                                    <Header.Content as='h1'>
+                                        Welcome to CryptoHaven<br />
+                                        the safe place for you<br />
+                                        and your portfolio needs
+                                </Header.Content>
+                                </Header>
+                            </Segment>
+                        </Grid.Column>
+                        <Grid.Column />
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column />
+                        <Grid.Column width={10}>
+                        <Button positive size='massive'>
+                            Sign Up
+                        </Button>
+                        <Button primary size='massive'>
+                            Demo
+                        </Button>
+                        </Grid.Column>
+                        <Grid.Column />
+                    </Grid.Row>
+                    <Grid.Row />
+                </Grid>
+            </ContentWrapper>
+        </div>
     )
 }
 
