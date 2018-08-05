6de0bf7b59291d787fda8810c9fd524d9dc39d10
Radomir Nowacki <kenderpl@gmail.com>
2018-08-06 01:26:30 +0200

    404 styling changes

--- src/components/NotFound404.js
+++ src/components/NotFound404.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Segment, Header } from 'semantic-ui-react'
+import { Segment, Header, Card } from 'semantic-ui-react'
 
 export default () => (
-    <div style={{ margin: "10rem auto" }}>
-        <Segment padded='very' inverted color='red'>
-            <Header as='h1' style={{ fontSize: "20rem" }} dividing >404</Header>
-            <Header.Subheader style={{ fontSize: "2rem", textAlign: "center" }}>Content not found</Header.Subheader>
-        </Segment>
+    <div style={{width: "100vw", backgroundColor: "rgb(27,28,29)"}}>
+        <div style={{ margin: "10rem auto", width: "45rem", border: "white 0.5rem solid"}}>
+            <Segment padded='very' inverted color='red' style={{borderRadius: 0}}>
+                <Header as='h1' style={{ fontSize: "20rem", textAlign: "center" }}>404</Header>
+                <Header.Subheader style={{ fontSize: "2rem", textAlign: "center" }}>Content not found</Header.Subheader>
+            </Segment>
+        </div>
     </div>
 )
